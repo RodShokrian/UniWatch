cc9fb0ce1e88fbf1fb81b5f757a4bd616a7c6095
rodshokrian <rodshokrian@gmail.com>
2017-11-13 18:08:27 -0800

    update endpoint for news API

--- frontend/util/api_util_news.js
+++ frontend/util/api_util_news.js
@@ -1,23 +1,14 @@
 export const fetchNews = (searchString) => (
   $.ajax({
     method: 'GET',
-    url: `https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=${searchString}&count=5`,
+    url: `https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=${searchString}&count=5`,
     headers: {
-      "Ocp-Apim-Subscription-Key": "3b9bc7de7d144851a7dd175275170d00"
+      "Ocp-Apim-Subscription-Key": "2156dee73fa9490b893a1786cd02db67"
     }
   })
 );
 // Bing News Search API Endpoint:
-// https://api.cognitive.microsoft.com/bing/v5.0/images
+// https://api.cognitive.microsoft.com/bing/v7.0/images
 // API Keys:
-// Key 1: 3b9bc7de7d144851a7dd175275170d00
-// Key 2: ac2080c8a7e9487ea4c441b0bfe5a86b
-export const newsTest = () => (
-  $.ajax({
-    method: 'GET',
-    url: `https://api.cognitive.microsoft.com/bing/v5.0/news/search?q=Stanford+University&mkt=en-us&count=10`,
-    headers: {
-      "Ocp-Apim-Subscription-Key": "3b9bc7de7d144851a7dd175275170d00"
-    }
-  })
-);
+// Key 1: 2156dee73fa9490b893a1786cd02db67
+// Key 2: 47cb35d2c1e24b748cf8d8e6ce2c9325
