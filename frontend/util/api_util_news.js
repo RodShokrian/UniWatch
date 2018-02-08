23da8376eccf9c499e779f9025560caece903f55
RodShokrian <rodshokrian@gmail.com>
2018-02-07 18:32:01 -0800

    Update keys

--- frontend/util/api_util_news.js
+++ frontend/util/api_util_news.js
@@ -3,12 +3,12 @@ export const fetchNews = (searchString) => (
     method: 'GET',
     url: `https://api.cognitive.microsoft.com/bing/v7.0/news/search?q=${searchString}&count=5`,
     headers: {
-      "Ocp-Apim-Subscription-Key": "2156dee73fa9490b893a1786cd02db67"
+      "Ocp-Apim-Subscription-Key": "596b88d54f6b4b84a277acfb65a6fbc6"
     }
   })
 );
 // Bing News Search API Endpoint:
 // https://api.cognitive.microsoft.com/bing/v7.0/images
 // API Keys:
-// Key 1: 2156dee73fa9490b893a1786cd02db67
-// Key 2: 47cb35d2c1e24b748cf8d8e6ce2c9325
+// Key 1: 596b88d54f6b4b84a277acfb65a6fbc6
+// Key 2: 39f6e859bdbb4bbc976566e9da2a33f5
